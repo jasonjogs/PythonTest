f2f7fb70048e4ad49e0e85a56257ad8883aaeeca
jason <jason.jia.chen@gmail.com>
2020-08-19 21:18:18 -0700

    changed assert value

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ class TestSum(unittest.TestCase):
     def test_list_fraction(self):
         data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
         result = sum(data)
-        self.assertEqual(result, 1)
+        self.assertEqual(result, 0.9)
     
     def test_bad_type(self):
         data = 1
